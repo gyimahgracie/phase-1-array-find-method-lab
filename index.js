669f882a8be1bf88a6d9d3f16ab012e79465fb13
gracie gyimah <graciegyimah9@gmail.com>
2023-04-15 15:46:57 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+//const record = [
+  //  { year: "2015", result: "W"},
+//    { year: "2014", result: "N/A"},
+//   { year: "2013", result: "L"}
+//     //...
+//   ]
+
+function superbowlWin(record) {
+   let variable = record.find ((element => element.result === "W" ))
+//record.find (element)
+console.log (variable)
+if (variable === undefined)
+{return undefined}
+else {return variable.year}
+
+
+}
+
+
+
+function ifStatements (element){
+    if (element.result === "W"){
+        return element.year }
+     if (element.result != "W"){
+         return undefined
+     }}
+
+
+superbowlWin()
+
